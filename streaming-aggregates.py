e04e128a369b4361c314141f1a4f75436ce4d4c2
jjcc0410 <jjcc0410>
2024-11-17 18:08:02 +0000

    added gold layer

--- streaming-aggregates.py
+++ streaming-aggregates.py
@@ -34,3 +34,35 @@ class Bronze():
                     ) 
         print("Done")
         return sQuery   
+
+# COMMAND ----------
+
+class Gold():
+    def __init__(self):
+        self.base_data_dir = "/FileStore/data_spark_streaming_scholarnest"
+        
+    def readBronze(self):
+        return spark.readStream.table("invoices_bz")
+
+    def getAggregates(self, invoices_df):
+        from pyspark.sql.functions import sum, expr
+        return (invoices_df.groupBy("CustomerCardNo")
+                    .agg(sum("TotalAmount").alias("TotalAmount"),
+                         sum(expr("TotalAmount*0.02")).alias("TotalPoints"))
+        )
+
+    def saveResults(self, results_df):
+        print(f"\nStarting Silver Stream...", end='')
+        return (results_df.writeStream
+                    .queryName("gold-update")
+                    .option("checkpointLocation", f"{self.base_data_dir}/chekpoint/customer_rewards")
+                    .outputMode("complete")
+                    .toTable("customer_rewards")
+                )
+        print("Done")
+
+    def process(self):
+        invoices_df = self.readBronze()
+        aggregate_df = self.getAggregates(invoices_df)
+        sQuery = self.saveResults(aggregate_df)
+        return sQuery
